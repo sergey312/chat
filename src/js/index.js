0afec264e1aeb71788101d573c5827c6b3d639a7
sergey312 <34268355+sergey312@users.noreply.github.com>
2019-07-01 00:53:51 +0300

    some upgreds

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,20 @@ import _ from 'lodash';
 
 
 import './../sass/styles.scss';
+function getDate()
+{
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    //По надобности условие ниже повторить с minutes и hours
+    if(seconds < 10)
+    {
+        seconds = '0' + seconds;
+    }
+    document.getElementById('timedisplay').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+setInterval(getDate, 0);
 
 // const getHeader = () => {
 //   const helloWebpack = _.join(['Hello', 'webpack!'], ' ');
